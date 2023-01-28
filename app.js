110c60499cd2543bbe22f723aa402bec95396582
Alejo <alejo.a11@outlook.es>
2023-01-28 05:33:56 -0300

    bootstrapcommit

---
+++ app.js
@@ -0,0 +1,32 @@
+function myDenji() {
+    
+    document.body.style.backgroundImage = "url('denji1.jpg')";
+  }
+  function myPochita() {
+    
+    document.body.style.backgroundImage = "url('pochita2.png')";
+    titulo.style.color="black"
+    
+  }
+  function myMakima() {
+    
+    document.body.style.backgroundImage = "url('makima1.jpg')";
+    titulo.style.color="white"
+
+  }
+  function myAki() {
+    
+    document.body.style.backgroundImage = "url('aki1.jpg')";
+    titulo.style.color="white"
+  }
+  function myHimeno() {
+    
+    document.body.style.backgroundImage = "url('himeno1.jpg')";
+    titulo.style.color="black"
+
+  }
+  function myPower() {
+    
+    document.body.style.backgroundImage = "url('power1.png')";
+    titulo.style.color="white"
+  }
